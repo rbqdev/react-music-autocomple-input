2b0cfc430101d5c9d5448a3f5a8b126e3ca0a481
rbqdev <rbq.dev@gmail.com>
2024-04-05 09:12:03 -0300

    feat: add HighLightWord component

---
+++ src/components/HighLightWord/HighLightWord.tsx
@@ -0,0 +1,19 @@
+type HighlightWordProps = {
+  textBase: string;
+  text: string;
+};
+
+export const HighlightWord = ({ textBase, text }: HighlightWordProps) => {
+  const parts = text.split(new RegExp(`(${textBase})`, "gi"));
+  return (
+    <p>
+      {parts.map((part, index) =>
+        part.toLowerCase() === textBase.toLowerCase() ? (
+          <mark key={`${part}-${index}`}>{part}</mark>
+        ) : (
+          <span key={`${part}-${index}`}> {part} </span>
+        )
+      )}
+    </p>
+  );
+};
